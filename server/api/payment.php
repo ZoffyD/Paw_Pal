857f0dabedf60321e1e03a69a16e91f095e07668
Eric Lee <leeshenyieric@gmail.com>
2026-01-09 17:30:46 +0800

    Update API keys and URLs in payment processing scripts

--- server/api/payment.php
+++ server/api/payment.php
@@ -8,8 +8,8 @@ $name = $_GET['name'];
 $amount = $_GET['amount']; 
 $userid = $_GET['userid'];
 
-$api_key = 'enter ur api key';
-$collection_id = 'enter your collection id'; 
+$api_key = 'a0bf3010-2b35-40a8-a81d-6362a0bd73fa';
+$collection_id = 'ckw3sauq'; 
 $host = 'https://www.billplz-sandbox.com/api/v3/bills';
 
 $data = array(
@@ -19,8 +19,8 @@ $data = array(
           'name' => $name,
           'amount' => $amount * 100, // Cents
           'description' => 'Wallet Topup',
-          'callback_url' => "https://domain.com/pawpal/server/api/return_url",
-          'redirect_url' => "https://domian.com/pawpal/server/api/payment_update.php?userid=$userid&email=$email&name=$name&phone=$phone&amount=$amount" 
+          'callback_url' => "https://canorcannot.com/Eric/pawpal/server/api/return_url",
+          'redirect_url' => "https://canorcannot.com/Eric/pawpal/server/api/payment_update.php?userid=$userid&email=$email&name=$name&phone=$phone&amount=$amount" 
 );
 
 $process = curl_init($host);
